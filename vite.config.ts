2e4624762d95475f124fd58314b74d8c7466dbf5
chan9yu <wry5533@gmail.com>
2023-10-19 00:33:27 +0900

    chore: 프로젝트 초기 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const userConfig = defineConfig({
+	base: "/",
+	server: {
+		host: "localhost",
+		port: 3035,
+	},
+	plugins: [],
+});
+
+export default userConfig;
