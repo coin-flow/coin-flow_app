2bc34238a1625b48b0189f35c9e1360f8312943e
chan9yu <wry5533@gmail.com>
2023-10-22 00:39:27 +0900

    feat: coin detail, dummy data를 이용한 페이지 구현

--- src/pages/coin/CoinListPage.tsx
+++ src/pages/coin/CoinListPage.tsx
@@ -1,5 +1,20 @@
+import { Grid } from '@mui/material';
+
+import { CoinItem } from '../../components';
+import { dummyBitcoinInfo } from '../../data';
+
 const CoinListPage = () => {
-	return <div>CoinListPage</div>;
+	return (
+		<div>
+			<Grid container spacing={2}>
+				{Object.entries(dummyBitcoinInfo).map(([key, value]) => (
+					<Grid key={key} item md={3} sm={6}>
+						<CoinItem price={value.closing_price} title={key} />
+					</Grid>
+				))}
+			</Grid>
+		</div>
+	);
 };
 
 export default CoinListPage;
