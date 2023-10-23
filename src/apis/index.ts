10dafc7977def6304810eee3df10f6789125a49d
ddanmuji <cgyeo5533@gmail.com>
2023-10-23 14:00:31 +0900

    style: 코드 정리

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,9 +1,1 @@
 export * from './test.api';
-
-const test = (string: string) => {
-	const isNotInt = string.includes('.');
-	if (isNotInt) return null;
-
-	const number = ~~string;
-	return number;
-};
