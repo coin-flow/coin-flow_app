401870ab2949a6d31acc8670828c35e892377292
chan9yu <wry5533@gmail.com>
2023-10-27 21:52:30 +0900

    style: logo 추가, 파비콘 추가, title 변경

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,19 +1,19 @@
 import { createTheme } from '@mui/material';
-import { blueGrey, deepPurple } from '@mui/material/colors';
+import { blueGrey, indigo } from '@mui/material/colors';
 
 export const darkTheme = createTheme({
 	palette: {
 		mode: 'dark',
-		primary: deepPurple,
-		secondary: deepPurple
+		primary: indigo,
+		secondary: indigo
 	}
 });
 
 export const lightTheme = createTheme({
 	palette: {
 		mode: 'light',
-		primary: deepPurple,
-		secondary: deepPurple,
+		primary: indigo,
+		secondary: indigo,
 		background: {
 			default: blueGrey['50']
 		}
