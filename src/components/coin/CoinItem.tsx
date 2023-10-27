62f5ac347753ceb6418e18d3ef9cd076afdea1c7
chan9yu <wry5533@gmail.com>
2023-10-27 20:54:49 +0900

    feat: bitcoin info card 실제 데이터 추가

--- src/components/coin/CoinItem.tsx
+++ src/components/coin/CoinItem.tsx
@@ -1,5 +1,4 @@
-import { Avatar, Button, Card, CardActions, CardContent, CardHeader, Typography } from '@mui/material';
-import { SyntheticEvent } from 'react';
+import { Avatar, Button, Card, CardContent, CardHeader, List, ListItem, ListItemText } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 import type { BitcoinData } from '../../@types';
@@ -9,13 +8,11 @@ interface CoinItemProps {
 	coinInfo: BitcoinData;
 }
 
-const formatter = new Intl.NumberFormat('ko-KR', {
-	style: 'currency',
-	currency: 'krw'
-});
-
 const formatPrice = (price: string) => {
+	const option: Intl.NumberFormatOptions = { style: 'currency', currency: 'krw' };
+	const formatter = new Intl.NumberFormat('ko-KR', option);
 	const formattedPrice = formatter.format(parseInt(price, 10));
+
 	return formattedPrice;
 };
 
@@ -46,28 +43,36 @@ const CoinItem = (props: CoinItemProps) => {
 	};
 
 	return (
-		<Card sx={{ minWidth: 275 }}>
+		<Card variant="outlined">
 			<CardHeader
 				avatar={<Avatar src={coinImg} alt={coin_name} />}
-				action={<Button aria-label="settings">Setting</Button>}
-				title="Shrimp and Chorizo Paella"
-				subheader="September 14, 2016"
+				action={
+					<Button variant="outlined" onClick={handleMoveToCoinDetail}>
+						상세보기
+					</Button>
+				}
+				title={coin_name}
+				subheader={coin_name}
 			/>
 			<CardContent>
-				<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-					coin detail page {coin_name}
-				</Typography>
-				<Typography variant="h5" component="div">
-					{coin_name}
-				</Typography>
-				<Typography sx={{ mb: 1.5 }} color="text.secondary">
-					coin detail
-				</Typography>
-				<Typography variant="body2">{formatPrice(max_price)}</Typography>
+				<List disablePadding>
+					<ListItem disablePadding>
+						<ListItemText sx={{ width: '50%' }} primary="종가(KRW)" secondary={`${formatPrice(closing_price)}`} />
+						<ListItemText
+							sx={{ width: '50%' }}
+							primary="최근 변동률 (24H)"
+							secondary={`${fluctate_rate_24H}%`}
+							secondaryTypographyProps={{
+								color: ({ palette }) => (Number(fluctate_rate_24H) >= 0 ? palette.info.main : palette.error.main)
+							}}
+						/>
+					</ListItem>
+					<ListItem disablePadding>
+						<ListItemText sx={{ width: '50%' }} primary="금일 최고가" secondary={`${formatPrice(max_price)}`} />
+						<ListItemText sx={{ width: '50%' }} primary="금일 최저가" secondary={`${formatPrice(min_price)}`} />
+					</ListItem>
+				</List>
 			</CardContent>
-			<CardActions onClick={handleMoveToCoinDetail}>
-				<Button size="small">Move To {coin_name} Detail Page</Button>
-			</CardActions>
 		</Card>
 	);
 };
